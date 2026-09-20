a8d97a42f20b500ca08c6669b27425566789d1c8
arman khachatryan <arman.khachatryan2125@gmail.com>
2026-09-20 17:49:46 +0000

    fixed task2

--- hw3/task2.c
+++ hw3/task2.c
@@ -4,40 +4,42 @@
 #include <sys/wait.h>
 
 int main() {
-	pid_t id = fork();
+	pid_t id1 = fork();
 
-	if (id < 0) {
-        	printf("Error: subprocess was not created\n");
-        	return 1;
-    	}
-    
-	if (id == 0) {
-        	printf("Child 1: id is %d\n", getpid());
-        	exit(1);
-   	} else { 
-        	pid_t parentid = getpid();
-        	wait(NULL);
-        	printf("Parent: id is %d\n", parentid);
-        	printf("Parent: waited until child %d is done\n", id); 
-    	}
+	if (id1 < 0) {
+		printf("Error: subprocess was not created\n");
+		return 1;
+	}
 
-    	pid_t id2 = fork();
+	if (id1 == 0) {
+		printf("Child 1: id is %d, exiting with 1\n", getpid());
+		exit(1);
+	}
 
-    	if (id2 < 0) {
-       		printf("Error: subprocess was not created\n");
-        	return 1;
-    	}
+	pid_t id2 = fork();
 
-    	if (id2 == 0) {
-        	printf("Child 2: id is %d\n", getpid());
-        	exit(1);
-    	} else {
-        	pid_t parentid = getpid();
-        	int status;
-        	waitpid(id2, &status, 0); 
-        	printf("Parent: id is %d\n", parentid);
-           	printf("Parent: waited until child %d is done with exit status %d\n", id2, status); 
-    	}
+	if (id2 < 0) {
+		printf("Error: subprocess was not created\n");
+		return 1;
+	}
 
-    	return 0;
+	if (id2 == 0) {
+		sleep(2);
+		printf("Child 2: id is %d, exiting with 2\n", getpid());
+		exit(2);
+	}
+
+	printf("Parent: id is %d\n", getpid());
+
+	int status2;
+	waitpid(id2, &status2, 0);
+	if (WIFEXITED(status2))
+		printf("Parent: waited for child %d, exit status %d\n", id2, WEXITSTATUS(status2));
+
+	int status1;
+	pid_t done = wait(&status1);
+	if (WIFEXITED(status1))
+		printf("Parent: waited for child %d, exit status %d\n", done, WEXITSTATUS(status1));
+
+	return 0;
 }
